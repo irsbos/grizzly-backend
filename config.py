e2b5aa12517a734dd91d9dfdb20945363af1eeef
irsbos <x4mohammad@gmail.com>
2026-06-09 01:29:24 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 # config.py
 # إعدادات المشروع العامة
 
-BOT_TOKEN = "YOUR_TELEGRAM_BOT_TOKEN"          # ضع توكن البوت الخاص بك من BotFather هنا
-GRIZZLY_API_KEY = "YOUR_GRIZZLY_API_KEY"      # ضع مفتاح الـ API الخاص بحسابك في Grizzly SMS هنا
+BOT_TOKEN = "8994234811:AAFisVC7KhXRx0512g9SBKa6Fm4xd8Y6qaM"          # ضع توكن البوت الخاص بك من BotFather هنا
+GRIZZLY_API_KEY = "f48a208f5d4128e28a470e98741a8f56"      # ضع مفتاح الـ API الخاص بحسابك في Grizzly SMS هنا
 PROFIT_MARGIN = 1.30                           # هامش الربح الخاص بك (مثال: 1.30 تعني زيادة 30% على السعر الأصلي)
 WEBAPP_URL = "https://yourdomain.com/index.html" # رابط واجهة الـ Mini App (الموقع) بعد رفعه أونلاين
