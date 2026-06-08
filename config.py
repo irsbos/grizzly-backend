9dc7447fbe405667c4c9629f9a918241a66f93ab
irsbos <x4mohammad@gmail.com>
2026-06-09 02:17:35 +0300

    Update config.py

--- config.py
+++ config.py
@@ -2,6 +2,6 @@
 # إعدادات المشروع العامة
 
 BOT_TOKEN = "8994234811:AAFisVC7KhXRx0512g9SBKa6Fm4xd8Y6qaM"          # ضع توكن البوت الخاص بك من BotFather هنا
-GRIZZLY_API_KEY = "f48a208f5d4128e28a470e98741a8f56"      # ضع مفتاح الـ API الخاص بحسابك في Grizzly SMS هنا
+GRIZZLY_API_KEY = "ad1a8bb14fe6411695db6630e3cb791b"      # ضع مفتاح الـ API الخاص بحسابك في Grizzly SMS هنا
 PROFIT_MARGIN = 1.30                           # هامش الربح الخاص بك (مثال: 1.30 تعني زيادة 30% على السعر الأصلي)
 WEBAPP_URL = "https://irsbos.github.io/NumBase/index.html" # رابط واجهة الـ Mini App (الموقع) بعد رفعه أونلاين
